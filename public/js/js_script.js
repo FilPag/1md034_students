7caccb78387f136192d28a700d2c6626ff9ea3d8
FilPag <Filip.pagliaro@student.uu.se>
2020-02-03 13:32:23 +0100

    4-2

---
+++ public/js/js_script.js
@@ -0,0 +1,21 @@
+function menuItem(name, kCal, descriptions, imgPath){
+  this.name = name;
+  this.kCal = kCal;
+  this.imgPath = imgPath;
+  this.descriptions = descriptions;
+  this.string = function() {
+    return "Namn: " + this.name + " - kCal: " + this.kCal;
+  };
+}
+
+let burg1 = new menuItem("The CheeseBurger", "200 kCal", "(Contains lactose)", "img/cheeseburger.jpg");
+let burg2 = new menuItem("The Open Cheeseburger", "150 kCal", "Stays open", "img/openBurg.jpg");
+let burg3 = new menuItem("The Upside Down Cheeseburger", "345 kCal", "Void Engineered Bun (Gluten free)", "img/upsideDown.jpg");
+let burg4 = new menuItem("The big boy", "1000 kCal", "Pure meat", "todo");
+let burg5 = new menuItem("The SMALL boy", "1 kCal", "No meat", "todo");
+
+console.log(burg1.string());
+console.log(burg2.string());
+console.log(burg3.string());
+console.log(burg4.string());
+console.log(burg5.string());
