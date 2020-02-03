b00f69a19d8ecc73e94f200aa2dc2d652c63436e
FilPag <Filip.pagliaro@student.uu.se>
2020-02-03 22:31:52 +0100

    t4-8

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -10,11 +10,11 @@ function menuItem(name, kCal, otherInfo, imgPath){
 
 let menuArray = {}
 
-menuArray[0] = new menuItem("The CheeseBurger", "200 kCal", " Contains the finest cheese (Contains lactose)", "img/cheeseburger.jpg");
+menuArray[0] = new menuItem("The CheeseBurger", "200 kCal", " Contains the finest cheese (Contains lactose)", "img/cheeseburger.png");
 menuArray[1] = new menuItem("The Open Cheeseburger", "150 kCal", "Stays open", "img/openBurg.jpg");
 menuArray[2] = new menuItem("The Upside Down Cheeseburger", "345 kCal", "Void Engineered Bun (Gluten free)", "img/upsideDown.jpg");
-menuArray[3] = new menuItem("The big boy", "1000 kCal", "Pure meat", "todo");
-menuArray[4] = new menuItem("The SMALL boy", "1 kCal", "", "todo");
+menuArray[3] = new menuItem("The big boy", "1000 kCal", "Pure meat", "img/huge.jpeg");
+menuArray[4] = new menuItem("The SMALL boy", "1 kCal", "", "img/mini.jpeg");
 
 const vm = new Vue({
   el: '#burgers',
