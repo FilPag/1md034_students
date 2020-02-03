fb56e2b9f089ad6268d87dc6a7c5cf36cba1be35
FilPag <Filip.pagliaro@student.uu.se>
2020-02-03 22:21:14 +0100

    t4-8 normal JS

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -10,17 +10,21 @@ function menuItem(name, kCal, otherInfo, imgPath){
 
 var menuArray = [];
 
-menuArray[0] = new menuItem("The CheeseBurger", "200 kCal", "Contains the finest cheese (Contains lactose)", "img/cheeseburger.jpg");
+menuArray[0] = new menuItem("The CheeseBurger", "200 kCal", "Contains the finest cheese (Contains lactose)", "img/cheeseburger.png");
 menuArray[1] = new menuItem("The Open Cheeseburger", "150 kCal", "Stays open", "img/openBurg.jpg");
 menuArray[2] = new menuItem("The Upside Down Cheeseburger", "345 kCal", "Void Engineered Bun (Gluten free)", "img/upsideDown.jpg");
-menuArray[3] = new menuItem("The big boy", "1000 kCal", "Pure meat", "todo");
-menuArray[4] = new menuItem("The SMALL boy", "1 kCal", "", "todo");
+menuArray[3] = new menuItem("The big boy", "1000 kCal", "Pure meat", "img/huge.jpeg");
+menuArray[4] = new menuItem("The SMALL boy", "1 kCal", "", "img/mini.jpeg");
 
 let burgerMenu = document.getElementById("menu");
 
 for (let i = 0; i < menuArray.length; ++i){
-  let burger= document.createElement("div");
+  let div = document.createElement("div");
   let burgerName = document.createTextNode(menuArray[i].name);
+  let img = document.createElement("img");
+  img.src = menuArray[i].imgPath;
+  img.className = "burgimg";
+
   let ul = document.createElement("ul");
 
   let kCalElem = document.createElement('li');
@@ -30,7 +34,13 @@ for (let i = 0; i < menuArray.length; ++i){
   otherInfo.appendChild(document.createTextNode(menuArray[i].otherInfo));
 
   ul.appendChild(kCalElem);
-  burger.appendChild(burgerName);
-  burger.appendChild(ul)
-  burgerMenu.appendChild(burger);
+    
+  if(menuArray[i].otherInfo != ""){
+    ul.appendChild(otherInfo);
+  }
+
+  div.appendChild(burgerName);
+  div.appendChild(img);
+  div.appendChild(ul);
+  burgerMenu.appendChild(div);
 }
