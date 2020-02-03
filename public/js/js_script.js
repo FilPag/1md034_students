268e50069dcbecccd3bfb146c97ba1aef087590c
FilPag <Filip.pagliaro@student.uu.se>
2020-02-03 23:09:10 +0100

    t4-9

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -1,41 +1,24 @@
-function menuItem(name, kCal, otherInfo, imgPath){
-  this.name = name;
-  this.kCal = kCal;
-  this.imgPath = imgPath;
-  this.otherInfo= otherInfo;
-  this.string = function() {
-    return this.name
-  };
-}
-
-var menuArray = [];
-
-menuArray[0] = new menuItem("The CheeseBurger", "200 kCal", "Contains the finest cheese (Contains lactose)", "img/cheeseburger.png");
-menuArray[1] = new menuItem("The Open Cheeseburger", "150 kCal", "Stays open", "img/openBurg.jpg");
-menuArray[2] = new menuItem("The Upside Down Cheeseburger", "345 kCal", "Void Engineered Bun (Gluten free)", "img/upsideDown.jpg");
-menuArray[3] = new menuItem("The big boy", "1000 kCal", "Pure meat", "img/huge.jpeg");
-menuArray[4] = new menuItem("The SMALL boy", "1 kCal", "", "img/mini.jpeg");
 
 let burgerMenu = document.getElementById("menu");
 
-for (let i = 0; i < menuArray.length; ++i){
+for (let i = 0; i < food.length; ++i){
   let div = document.createElement("div");
-  let burgerName = document.createTextNode(menuArray[i].name);
+  let burgerName = document.createTextNode(food[i].name);
   let img = document.createElement("img");
-  img.src = menuArray[i].imgPath;
+  img.src = food[i].imgPath;
   img.className = "burgimg";
 
   let ul = document.createElement("ul");
 
   let kCalElem = document.createElement('li');
-  kCalElem.appendChild(document.createTextNode(menuArray[i].kCal));
+  kCalElem.appendChild(document.createTextNode(food[i].kCal));
 
   let otherInfo = document.createElement('li');
-  otherInfo.appendChild(document.createTextNode(menuArray[i].otherInfo));
+  otherInfo.appendChild(document.createTextNode(food[i].otherInfo));
 
   ul.appendChild(kCalElem);
-    
-  if(menuArray[i].otherInfo != ""){
+
+  if(food[i].otherInfo != ""){
     ul.appendChild(otherInfo);
   }
 
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,24 +1,6 @@
-function menuItem(name, kCal, otherInfo, imgPath){
-  this.name = name;
-  this.kCal = kCal;
-  this.imgPath = imgPath;
-  this.otherInfo= otherInfo;
-  this.string = function() {
-    return this.name + '\n kCal: ' + this.kCal
-  };
-}
-
-let menuArray = {}
-
-menuArray[0] = new menuItem("The CheeseBurger", "200 kCal", " Contains the finest cheese (Contains lactose)", "img/cheeseburger.png");
-menuArray[1] = new menuItem("The Open Cheeseburger", "150 kCal", "Stays open", "img/openBurg.jpg");
-menuArray[2] = new menuItem("The Upside Down Cheeseburger", "345 kCal", "Void Engineered Bun (Gluten free)", "img/upsideDown.jpg");
-menuArray[3] = new menuItem("The big boy", "1000 kCal", "Pure meat", "img/huge.jpeg");
-menuArray[4] = new menuItem("The SMALL boy", "1 kCal", "", "img/mini.jpeg");
-
 const vm = new Vue({
   el: '#burgers',
   data: {
-    menuArray: menuArray
+    menuArray: food
   }
 });
