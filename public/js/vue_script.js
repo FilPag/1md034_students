9031ee7f68d159397dd9c53ae3f6fd0b1dc89929
FilPag <Filip.pagliaro@student.uu.se>
2020-02-04 18:13:03 +0100

    t4-10

--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,6 +1,31 @@
 const vm = new Vue({
   el: '#burgers',
   data: {
+    ordered: [],
     menuArray: food
+  }
+});
+
+const send = new Vue({
+  el: '#customerinfo',
+  data:{
+    result:"",
+    firstname:"",
+    mail:"",
+    street:"",
+    payment:"Credit card",
+    picked:"undisclosed"
+  },
+  methods:{
+    send: function(){
+      let res = [];
+      res[0] = this.firstname;
+      res[1] = this.mail;
+      res[2] = this.street;
+      res[3] = this.payment;
+      res[4] = this.picked;
+      res[5] = vm.ordered.toString();
+      this.result = res;
+    }
   }
 });
